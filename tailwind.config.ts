8b2b5324abe1df9ee17a42416bb1fe24a54e3503
eruiz <emilianordx@gmail.com>
2023-08-27 16:34:12 -0300

    refactor(tailwind): added project font on tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        josefin: "var(--font-josefin-sans)",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
